13237adb00e593f7e170d4364a2630020c6b737c
Risto Hinno <ristohinno@gmail.com>
2015-10-04 16:58:25 +0300

    Üleminek python 3.4le, koodi täiendamine

--- veebikraapimine.py
+++ veebikraapimine.py
@@ -4,102 +4,37 @@ Created on Thu Oct 01 08:09:56 2015
 
 @author: risto.hinno
 """
-
-#import BeautifulSoup
+#Python 3.4.3
 from bs4 import BeautifulSoup
-import urllib2
-#utf8 jaoks, et urle saaks loopida
-import sys  
-reload(sys)  
-sys.setdefaultencoding('utf8')
-#regrex
-import re 
-#esialgne
-for i in range(1,2):
-    url= "http://izzi.ee/catalog/category/Ettevõtted/page/%s" %i
-    response=urllib2.urlopen(url)
+import urllib.request
+
+#avab tühja faili, loobib läbi URLid ning kirjutab faili ettevõtte nime,
+# regkoodi, e-maili, telefoni, kodulehe ja aadressi 
+fail = open("ettevotted.txt", "w")
+for i in range(1,3):
+    url= "http://izzi.ee/catalog/category/Ettev%%C3%%B5tted/page/%s" %i    
+    response=urllib.request.urlopen(url)   
     html=response.read()
     soup=BeautifulSoup(html)
-    divclass2=soup.find_all("div", attrs={"class":"cat_result_description"})
-    for i in divclass2:
-        desc=i.get_text()
-        listitem=desc.split(",")
-        print listitem[0]+","+listitem[1]
-
-
-#proovin koos e-mailiga ja aadressiga andmeid kätte saada 
-file = open("newfile.txt", "w")
-              
-for i in range(1,2):
-    url= "http://izzi.ee/catalog/category/Ettevõtted/page/%s" %i
-    response=urllib2.urlopen(url)
-    html=response.read()
-    soup=BeautifulSoup(html)
-    divclass2=soup.find_all("div", attrs={"class":"cat_result_description"})
-    for i in divclass2:
-        desc=i.get_text()
-        listitem=desc.split(",")
-        u=desc.encode("utf-8")
-        if u.find("@")!=-1 and len(listitem)==4:
-            file.write(listitem[0]+", "+listitem[1]+", "+\
-            re.split("e-mail: ", u)[-1]+", "+listitem[2]+"\n")
-        elif u.find("@")!=-1 and len(listitem)>4:
-            file.write(listitem[0]+", "+listitem[1]+", "+\
-            re.split("e-mail: ", u)[-1]+", "+listitem[2]+", "+\
-            listitem[3]+"\n")
-        elif u.find("@")==-1:
-            file.write(listitem[0]+", "+listitem[1]+",     "+\
-            ", "+listitem[2]+"\n")
-            
-file.close()
-
-#vana ja töötab  
-for i in range(1,2):
-    url= "http://izzi.ee/catalog/category/Ettevõtted/page/%s" %i
-    response=urllib2.urlopen(url)
-    html=response.read()
-    soup=BeautifulSoup(html)
-    divclass2=soup.find_all("div", attrs={"class":"cat_result_description"})
-    for i in divclass2:
-        desc=i.get_text()
-        listitem=desc.split(",")
-        u=desc.encode("utf-8")
-        if u.find("@")!=-1 and len(listitem)==4:
-            print listitem[0]+","+listitem[1]+","+re.split("e-mail: ", u)[-1]+\
-            ","+listitem[2]
-        elif u.find("@")!=-1 and len(listitem)>4:
-            print listitem[0]+","+listitem[1]+","+re.split("e-mail: ", u)[-1]+\
-            ","+listitem[2]+","+listitem[3]
-        elif u.find("@")==-1:
-            print listitem[0]+","+listitem[1]+", "+\
-            ","+listitem[2]
-        
-#lühemalt   
-def andmed(soup):
-    """soup->kirjutab listi txt faili
-    annad sisse veebilehe soup ning tagastab listi ettevõtte nimest, 
-    regkoodist ja aadressist, kirjutab selle txt faili
-    Kirjutab andmeid faili selliselt:
-    11914364, 2WAY OÜ
-    11524002, 2WTEAM OÜ
-    """
-    divclass2=soup.find_all("div", attrs={"class":"cat_result_description"})
-    for i in divclass2:
-        desc=i.get_text()
-        listitem=desc.split(",")
-        #file.write(listitem[1]+", "+listitem[0]+"\n")
-        file.write(listitem[0]+","+listitem[1]+"\n")
+    divclass=soup.find_all("div", attrs={"class":"cat_result_description"})
+    for i in divclass:
+        desc=i.get_text() 
+        listitem=desc.split(",") #teeb tekstist listi
+        #ükshaaval kirjutab elemendid faili ja eemaldab listist
+        fail.write(listitem.pop(0) + ", " + listitem.pop(0) + ", " +\
+        abifunktsioon("e-mail:") + ", " + abifunktsioon("tel:") + ", " +\
+        abifunktsioon("www.") + ", " + "".join(listitem) + "\n")
  
-file = open("newfile.txt", "w")
-for i in range(1,5):
-    url= "http://izzi.ee/catalog/category/Ettevõtted/page/%s" %i
-    response=urllib2.urlopen(url)
-    html=response.read()
-    soup=BeautifulSoup(html)
-    andmed(soup)
-
-file.close()
-
-
-
-
+fail.close()
+                 
+def abifunktsioon(kanal):
+    """kanal(str)->str (kui vastav kanal olemas siis listist listitem tagastab 
+    vastava elemendi (kanali) väärtuse, muidu tagastab tühja stringi " ") 
+    """
+    if len([s for s in listitem if kanal in s])!=0: 
+        return listitem.pop(listitem.index("".join([s for s in listitem if kanal in s])))
+    else:
+        return" "
+          
+        
+            
