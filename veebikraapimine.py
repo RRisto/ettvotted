cdf0da7a8f1ba604d65aca344d40bc09331b808f
KRaudla <kasparraudla@gmail.com>
2015-10-07 22:14:09 +0300

    page 6 problem solved
    the new way.

--- veebikraapimine.py
+++ veebikraapimine.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#Versioon: 0.0.4
-#Kuupaev: 05.10.2015
+#Versioon: 0.0.6
+#Kuupaev: 07.10.2015
 
 import urllib.request
 import csv
 from bs4 import BeautifulSoup
 import time
-def abifunktsioon(kanal):
-    """kanal(str)->str (kui vastav kanal olemas siis listist listitem tagastab
-    vastava elemendi (kanali) väärtuse, muidu tagastab tühja stringi " ")
-    """
-    if len([s for s in listitem if kanal in s])!=0:
-        return listitem.pop(listitem.index("".join([s for s in listitem if kanal in s])))
-    else:
-        return" "
-
+import re
 with open("data.csv","w", encoding="utf-8") as output_file:
     writer = csv.writer(output_file, delimiter="\t",quoting=csv.QUOTE_ALL)
-    for i in range (6,1000000):
+    for i in range (6,7):
         url = "http://izzi.ee/catalog/category/Ettev%C3%B5tted/page/{}".format(i)
         time.sleep(1)
         response =urllib.request.urlopen(url)
@@ -27,12 +19,21 @@ with open("data.csv","w", encoding="utf-8") as output_file:
         for i in soup.find_all("div", attrs={"class": "cat_result_description"}):
             desc=i.get_text()
             listitem=desc.split(",") #teeb tekstist listi
-            print (listitem)
-            #ükshaaval kirjutab elemendid faili ja eemaldab listist
-            row = [listitem.pop(0),listitem.pop(0),abifunktsioon("e-mail:"), abifunktsioon("tel:"), \
-            abifunktsioon("www."),"".join(listitem),url]
+            try:
+
+                nimi = listitem[0]
+                regkood = listitem[1]
+                email = re.findall(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",desc)
+                emailclean = ",".join(email)
+                telefon372 = re.findall("3725\d{6,7}",desc)
+                telefon372clean = ",".join(telefon372)
+                telefon = re.findall("5\d{6,7}",desc)
+                telefonclean = ",".join(telefon)
+                aadress = re.findall('1\d{7},\s(.*?)tel:|1\d{7},\s(.*?)e-mail:|1\d{7},\s(.*?)$',desc)
+                aadressclean= "".join(aadress[0])
+            except:
+                print ("error: ")
+            row = (nimi,regkood,aadressclean,emailclean,telefonclean,telefon372clean)
+
             writer.writerow([tulp.strip() for tulp in row])
-            #print (row)
-
-
-
+            print (row)
