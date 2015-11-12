6f9d0af642695875495767e90be2e9574ed00822
Risto Hinno <ristohinno@gmail.com>
2015-11-12 19:10:48 +0200

    parandasin loopimise vea

--- maaamet_geocoding.py
+++ maaamet_geocoding.py
@@ -1,33 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  4 18:47:37 2015
-
-@author: Risto
-"""
 import os
-os.chdir(r"C:\Users\Risto\Documents\Python Scripts")
-#geokodeerimine maa-ameti rakenduse kaudu
-import pandas as pd
-data=pd.read_csv("31_10_2015_arireg.csv", sep=";", encoding="latin-1")
+os.chdir(r"C:\Users\Risto\Documents\GitHub\GeoJson--riregistri-andmetest")
 
-#liidan aadressi ühte lahtrisse kokku
-data["kogu_aadress"]=data["asukoht_ettevotja_aadressis"]+" "+\
-data["asukoha_ehak_tekstina"]
-#loobin läbi ja lisan longi ja lati
+import pandas
+from geopy.geocoders import Nominatim
 import requests
 import json
-aadressid=data["kogu_aadress"][1:3]
-for i in range(0,len(data["kogu_aadress"])):
-for i in range(0,5):   
-    aadress=data["kogu_aadress"][i]
-    url="http://inaadress.maaamet.ee/inaadress/gazetteer?callback=v&appartment=1&address="+\
-    aadress
-    cont=requests.get(url).content.decode("utf-8")
-    cont=str(cont).strip("'<>( ) 'v") #muidu ei saa aru, et on json
-    parsedJson=json.loads(cont)
-    data["lat"][i]=parsedJson["addresses"][0]["viitepunkt_y"]
-    data["lon"][i]=parsedJson["addresses"][0]["viitepunkt_x"]
-    
+import time
+
+
+data=pandas.read_csv("maaamet_arireg.csv", sep=";", encoding="latin-1")
+#vaja teha ainult esimesele sisselugemisel
+#data["aadressid"] = data["asukoht_ettevotja_aadressis"]+","+data["asukoha_ehak_tekstina"]
+#data["viitepunkt_y"]=""
+#data["viitepunkt_x"]=""
+#data["taisaadress"]=""
+
+#
+alguskoht=len(data["viitepunkt_y"])-data["viitepunkt_y"].isnull().sum()
+#for i in range(alguskoht,len(data["aadressid"])):
+for i in range(alguskoht,100):    
+        url = "http://inaadress.maaamet.ee/inaadress/gazetteer?address="+data["aadressid"][i]
+        r = requests.get(url)
+        j = r.content
+        json_str = j.decode("utf-8")
+        parsed_json = json.loads(json_str)
+        try:
+            adresses = parsed_json["addresses"][0]
+            url="http://www.maaamet.ee/rr/geo-lest/url/?xy="+adresses["viitepunkt_x"]+","+ adresses["viitepunkt_y"]
+            try:            
+                j = requests.get(url).content.decode("utf-8")
+                data.loc[i,"viitepunkt_x"]=j.split(",")[0]
+                data.loc[i,"viitepunkt_y"]=j.split(",")[1]
+                data.loc[i, "taisaadress"] = adresses["taisaadress"]
+                #row = (x+";"+y+";"+taisaadress)
+                #output.write(row+"\n")
+            except:
+                #row = ("NA"+";"+"NA"+";"+taisaadress)
+                #output.write(row+"\n")
+                data.loc[i,"viitepunkt_x"]="NA"
+                data.loc[i,"viitepunkt_y"]="NA"
+                data.loc[i, "taisaadress"]="NA"
+                continue
+        except:
+            data.loc[i,"viitepunkt_x"]="NA"
+            data.loc[i,"viitepunkt_y"]="NA"
+            data.loc[i, "taisaadress"]="NA"
+            continue
+        
+
+#saveime
+data.to_csv("maaamet_arireg.csv", sep=";")
+
+
     
 
     
